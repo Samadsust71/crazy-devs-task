4dfd21797e6b8080783e0141b55ce7c821cfd5eb
Samadsust71 <samadsust71@gmail.com>
2025-03-15 04:07:07 +0600

    file upload section css done

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -43,7 +43,7 @@ const FileUpload = () => {
         </div>
       </div>
 
-      <div className="text-[54px] font-medium text-center mb-2 flex items-center gap-2 justify-center">
+      <div className="text-[54px] font-medium text-center flex items-center gap-2 justify-center">
         <span>Add Files to</span>
         <div className="">
           <img src={cloud} alt="cloud" />
@@ -51,7 +51,7 @@ const FileUpload = () => {
         <span>Cloud Chat</span>
       </div>
 
-      <p className="text-center text-text-secondary text-sm font-semibold">
+      <p className="text-center text-text-secondary text-sm font-semibold mt-6">
         Your files will not be stored on our servers and no AI models will be
         trained. <br />
         <span className="text-accent-orange3">
@@ -60,9 +60,9 @@ const FileUpload = () => {
       </p>
 
       {/* File Upload & URL Input Section */}
-      <div>
-      <div className="grid grid-cols-2 gap-4 mt-6">
-        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-4">
+      <div className="mt-[60px]">
+      <div className="grid grid-cols-2 gap-2">
+        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-[18.5px] h-[120px]">
           <img src={mask} alt="mask" />
           <p className="text-lg font-semibold">
             Drag & drop local files here,
@@ -70,7 +70,7 @@ const FileUpload = () => {
           </p>
         </div>
 
-        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-4">
+        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-[18.5px] h-[120px]">
           <img src={url} alt="url" />
           <div className="">
             <p className="text-lg font-semibold">Enter in a public URL:</p>
@@ -85,28 +85,28 @@ const FileUpload = () => {
             </div>
           </div>
         </div>
-        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-4">
+        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-[18.5px] h-[120px]">
           <img src={dropbox} alt="drop-boc" />
-          <p className="text-lg font-semibold">Add files from Dropbox</p>
+          <p className="text-lg font-semibold ">Add files from Dropbox</p>
         </div>
-        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-2">
+        <div className="bg-[#242424] p-6 rounded-[20px]  cursor-pointer flex items-center gap-[18.5px] h-[120px]">
           <img src={drive} alt="drive" />
-          <p className="text-lg font-semibold">Add files from Google Drive</p>
+          <p className="text-lg font-semibold ">Add files from Google Drive</p>
         </div>
       </div>
       {/* Selected Files List */}
-      <div className="bg-[#242424] p-4 mt-4 rounded-[34px] flex items-center gap-3">
+      <div className="bg-[#242424] p-4 mt-5 rounded-[34px] flex items-center gap-2">
         {files.map((file, index) => (
           <div
             key={index}
-            className="flex items-center bg-[#2B2B2B] rounded-full px-2 w-fit gap-3 "
+            className="flex items-center bg-[#2B2B2B] rounded-full px-3 py-[7px]  w-fit gap-3 "
           >
             <div className="flex items-center gap-2">
               <FileText
                 className={` w-4 h-4 ${
                   file.name === "Report_file.pdf"
-                    ? "text-red-400"
-                    : "text-blue-400"
+                    ? "text-[#f76638]"
+                    : "text-[#508ef5]"
                 }`}
               />
               <p className="text-sm">{file.name}</p>
@@ -115,7 +115,7 @@ const FileUpload = () => {
               <Checkbox className="border border-[#7C7C7C] rounded-[6px]" />
               <span>Force OCR</span>
               <IoIosInformation className="bg-[#585858] w-4 h-4 rounded-full text-text-secondary/90" />
-              <X className="w-4 h-4 cursor-pointer" />
+              <X className="w-4 h-4 cursor-pointer text-text-secondary" />
             </div>
           </div>
         ))}
@@ -125,22 +125,22 @@ const FileUpload = () => {
       
 
       {/* Buttons */}
-      <div className="flex justify-center items-center gap-4 mt-4">
+      <div className="flex justify-center items-center gap-4 mt-[60px]">
         <Button
           variant="outline"
-          className="border border-accent-orange3 bg-transparent  text-text w-60 rounded-[56px] hover:bg-transparent"
+          className="border border-accent-orange3 bg-transparent h-[60px] text-text w-60 rounded-[56px] hover:bg-transparent"
         >
           Start
         </Button>
-        <span>or</span>
-        <Button className="bg-gradient-to-b from-[#FFBF00] to-[#FB9937] text-text font-medium hover:opacity-90 w-60 rounded-[56px]">
+        <span className="font-semibold">or</span>
+        <Button className="bg-gradient-to-b from-[#FFBF00] to-[#FB9937] h-[60px] text-text font-medium hover:opacity-100 w-60 rounded-[56px]">
         <HiSparkles className="text-3xl"/><span>Start with Deep Dive</span>
         </Button>
         <div>
-          <TooltipProvider>
+          <TooltipProvider delayDuration={100}>
             <Tooltip>
               <TooltipTrigger className="flex justify-center items-center">
-                <IoIosInformation  className="bg-white rounded-full text-black border-none outline-none text-2xl" />
+                <IoIosInformation  className="bg-white/30 hover:bg-white rounded-full text-black border-none outline-none text-2xl" />
               </TooltipTrigger>
               <TooltipContent>
                 <div className="text-white w-[226px] text-xs">
